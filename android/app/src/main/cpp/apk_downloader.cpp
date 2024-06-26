a1c619a17f34ccb186113807e282a7124cc307a2
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-06-26 23:03:09 +0530

    Bonk

--- android/app/src/main/cpp/apk_downloader.cpp
+++ android/app/src/main/cpp/apk_downloader.cpp
@@ -6,6 +6,12 @@
 
 extern "C" {
 
+struct ProgressCallbackInfo {
+    JNIEnv* env;
+    jobject progressCallback;
+    jobject onCompleteCallback;
+};
+
 size_t writeData(void* ptr, size_t size, size_t nmemb, void* stream) {
     std::ofstream* file = static_cast<std::ofstream*>(stream);
     file->write(static_cast<char*>(ptr), size * nmemb);
@@ -13,27 +19,28 @@ size_t writeData(void* ptr, size_t size, size_t nmemb, void* stream) {
 }
 
 int progressCallback(void* ptr, double totalToDownload, double nowDownloaded, double, double) {
-    JNIEnv* env = static_cast<JNIEnv*>(ptr);
-    jclass callbackClass = env->FindClass("io/github/troppical/network/APKDownloader$ProgressCallback");
+    ProgressCallbackInfo* callbackInfo = static_cast<ProgressCallbackInfo*>(ptr);
+    JNIEnv* env = callbackInfo->env;
+    jobject progressCallback = callbackInfo->progressCallback;
+    jclass callbackClass = env->GetObjectClass(progressCallback);
     jmethodID onProgressMethod = env->GetMethodID(callbackClass, "onProgress", "(I)V");
 
     if (totalToDownload > 0 && onProgressMethod) {
         int progress = static_cast<int>((nowDownloaded / totalToDownload) * 100);
-        jobject callbackObj = env->AllocObject(callbackClass);
-        env->CallVoidMethod(callbackObj, onProgressMethod, progress);
-        env->DeleteLocalRef(callbackObj);
+        env->CallVoidMethod(progressCallback, onProgressMethod, progress);
     }
     return 0;
 }
 
 JNIEXPORT jboolean JNICALL
-Java_io_github_troppical_network_APKDownloader_download(JNIEnv* env, jobject, jstring url, jstring outputFile, jobject progressCallback) {
+Java_io_github_troppical_network_APKDownloader_download(JNIEnv* env, jobject, jstring url, jstring outputFile, jobject progressCallback, jobject onCompleteCallback) {
     const char* cUrl = env->GetStringUTFChars(url, nullptr);
     const char* cOutputFile = env->GetStringUTFChars(outputFile, nullptr);
 
     CURL* curl;
     CURLcode res;
     std::ofstream file;
+    ProgressCallbackInfo callbackInfo{env, progressCallback, onCompleteCallback};
 
     curl = curl_easy_init();
     if (curl) {
@@ -44,7 +51,7 @@ Java_io_github_troppical_network_APKDownloader_download(JNIEnv* env, jobject, js
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
         curl_easy_setopt(curl, CURLOPT_PROGRESSFUNCTION, progressCallback);
-        curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, env);
+        curl_easy_setopt(curl, CURLOPT_PROGRESSDATA, &callbackInfo);
 
         res = curl_easy_perform(curl);
 
@@ -53,6 +60,11 @@ Java_io_github_troppical_network_APKDownloader_download(JNIEnv* env, jobject, js
         
         env->ReleaseStringUTFChars(url, cUrl);
         env->ReleaseStringUTFChars(outputFile, cOutputFile);
+        
+        // Call onComplete callback
+        jclass callbackClass = env->GetObjectClass(onCompleteCallback);
+        jmethodID onCompleteMethod = env->GetMethodID(callbackClass, "onComplete", "(Z)V");
+        env->CallVoidMethod(onCompleteCallback, onCompleteMethod, res == CURLE_OK ? JNI_TRUE : JNI_FALSE);
         
         return res == CURLE_OK ? JNI_TRUE : JNI_FALSE;
     }
